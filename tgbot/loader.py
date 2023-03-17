7135b9d6a829b24859b548d365cd668be1e58d2a
Alex_Trav <nerezov03@mail.ru>
2023-03-17 15:48:55 +0600

    Реализовал получения ключа через переменную окружения.

--- tgbot/loader.py
+++ tgbot/loader.py
@@ -1,7 +1,12 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from tgbot.variables.config import get_token_api
+
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
 
 
-bot = Bot(token=get_token_api())
+load_dotenv(find_dotenv())
+
+
+bot = Bot(token=getenv('BOT_TOKEN_API'))
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
--- tgbot/variables/config.py
+++ tgbot/variables/config.py
@@ -1,10 +1,3 @@
-from tgbot.db.database import db
-
-
-def get_token_api() -> str:
-    return db.get_data(table='config')[0][0]
-
-
 class Documents:
 
     new_count = 0
